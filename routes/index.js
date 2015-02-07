196ab3c5c3e50e03fb9f061674c6055a4e1b9e92
Cord Slatton-Valle <cord42@gmail.com>
2015-02-07 22:02:00 +0000

    index.js work

--- routes/index.js
+++ routes/index.js
@@ -5,40 +5,20 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
   //setup serial port to read RFID
-  var serialport = require("serialport");
-  var SerialPort = serialport.SerialPort; // localize object constructor
-  var sp = new SerialPort("/dev/ttyAMA0", {
-    baudrate: 9600,
-    parser: serialport.parsers.raw
-  });
-  //open serial port
-  sp.on("open", function () {
-    console.log('open');
-    //on data received
-    sp.on('data', function(data) {
-      //read as utf8
-      var buff = new Buffer(data, 'utf8');
-      //convert to hex
-      var encoded_hex = buff.toString('hex');
-      //convert to integer
-      var encoded_int = parseInt(encoded_hex, 16);
-      //console.log('data received: ' + data);
-      console.log('encoded hex data: ' + encoded_hex);
-      console.log('encoded int data: ' + encoded_int);
-    });
-  });
 
   var gpio = require('rpi-gpio');
 
   var pin   = 23;
   var delay = 2000;
   var count = 0;
-  var max   = 3;
+  var max   = 10;
 
   gpio.on('change', function(channel, value) {
     console.log('Channel ' + channel + ' value is now ' + value);
   });
-  gpio.setup(pin, gpio.DIR_OUT, on);
+  gpio.setup(23, gpio.DIR_OUT, off);
+  gpio.setup(24, gpio.DIR_OUT, on);
+  gpio.setup(25, gpio.DIR_OUT, on);
 
   function on() {
     if (count >= max) {
