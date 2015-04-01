69d043720e63b6160efe2a26e1a24bec0484aac1
Cord Slatton-Valle <cord@feynmangroup.com>
2015-04-01 14:19:01 -0700

    add unique to allowed tags array to prevent the same tag from being added again

--- app.js
+++ app.js
@@ -99,6 +99,7 @@ var Rfid = {
     },
     saveAllowedTag: function(tag){
         Rfid.allowedTags.push(tag);
+        Rfid.allowedTags = Toolbox.uniq(Rfid.allowedTags);
         var xml = Rfid.allowedTagsToXML();
         Rfid.saveAllowedTags(xml);
     },
@@ -237,6 +238,17 @@ var Toolbox = {
         if(Toolbox.debug){
             console.log(msg);
         }
+    },
+    uniq: function(a) {
+        var prims = {"boolean":{}, "number":{}, "string":{}}, objs = [];
+
+        return a.filter(function(item) {
+            var type = typeof item;
+            if(type in prims)
+                return prims[type].hasOwnProperty(item) ? false : (prims[type][item] = true);
+            else
+                return objs.indexOf(item) >= 0 ? false : objs.push(item);
+        });
     }
 };
 var Serial = {
